414c32f424d579ab788a8290a330bd10a9661fad
VTTR <7524825+VTTR@users.noreply.github.com>
2024-04-25 19:07:38 +0200

    added decrypt and encrypt

---
+++ dbeavercrypt.py
@@ -0,0 +1,35 @@
+import base64
+
+PASSWORD_ENCRYPTION_KEY = b"sdf@!#$verf^wv%6Fwe%$$#FFGwfsdefwfe135s$^H)dg"
+
+
+def encrypt(newpass: str) -> str:
+    ba : bytearray = bytearray(len(newpass) + 2)
+
+    for index, _ in enumerate(newpass):
+        ba[index] = ord(newpass[index])
+
+    ba[-1] = 129
+    ba[-2] = 0
+
+    output = bytearray(b"")
+    for i in range(len(ba)):
+        output.append(ba[i] ^ PASSWORD_ENCRYPTION_KEY[i % len(PASSWORD_ENCRYPTION_KEY)])
+    return base64.b64encode(output).decode('utf-8')
+
+
+def decrypt(password: str) -> str:
+    # OwEKLE4jpQ==    => Hello
+    data_bin = base64.b64decode(password)
+    output = bytearray(b"")
+
+    for i in range(len(data_bin)):
+        output.append(data_bin[i] ^ PASSWORD_ENCRYPTION_KEY[i % len(PASSWORD_ENCRYPTION_KEY)])
+
+    return output[:-2].decode('utf-8')
+
+
+# Test the encrypt function
+# password = "Hello"
+# encrypted_password = encrypt(password)
+# print(f"Das Ergebnis: {encrypted_password}")
